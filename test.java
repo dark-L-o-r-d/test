edc70f13c9dbd94ec2ec8108a5c9830e8d06a0d0
dark-L-o-r-d <sayantan2001bishayee@gmail.com>
2023-12-28 11:33:55 +0530

    Changed class name to Upper case

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-public class test {
+public class Test {
     public static void main(String args[]) {
         System.out.println("Learning github");
         System.out.println("First commit test");
